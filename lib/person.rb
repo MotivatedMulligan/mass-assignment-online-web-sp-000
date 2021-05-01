9e8ae7914ef9459ddcb725974f4b25e31d5a046f
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-05-01 21:34:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize()
+  end
+#   :name, :birthday, :hair_color, :eye_color, :height,
+# :weight, :handed, :complexion, :t_shirt_size,
+# :wrist_size, :glove_size, :pant_length, :pant_width
 end
+# will assign an arbitrary number of properties
+#on initialization
+
+# a different, shorter list of attributes
+# will also assign an arbitrary number of
+#properties on initialization
